759fb8a47eb2ae0de983a2a77a501f896b72479b
Boris Town <boristown@gmail.com>
2022-08-30 11:30:27 +0800

    Update translate.py

--- translate.py
+++ translate.py
@@ -3,5 +3,18 @@ import sys
  
 prePath = sys.argv[1]
 finPath = sys.argv[2]
- 
-copyfile(prePath, finPath)
+# read file
+def read_file(path):
+    lines = []
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f.readlines():
+            lines.append(line.strip())
+    return lines
+# write file
+def write_file(path, lines):
+    with open(path, 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(line + '')
+
+lines = read_file(prePath)
+write_file(finPath, lines)
