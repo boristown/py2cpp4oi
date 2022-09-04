1283a0ddf5653088f93369bb7bb103c5ee6d93ec
Boris Town <boristown@gmail.com>
2022-09-04 16:49:48 +0800

    Copilot更新

---
+++ convert.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Python3 Code:
+*/
+
+//Convert above Python code to C++ code
+//Using long long instead of int to avoid overflow
+//Don't forget to convert the Sorted() statement to C++ code
+//defines to reduce typing
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+//typedefs to reduce typing
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+//Copy Solution class here
